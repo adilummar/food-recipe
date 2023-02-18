90c08c9b272dd86345e2354599c6d8b3e8a9bc6f
adilummar <madilummer@gmail.com>
2023-02-18 15:27:21 +0530

    popular seted almost

---
+++ src/components/Veggi.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Veggi() {
+  return (
+    <div>
+      veggi
+    </div>
+  )
+}
+
+export default Veggi
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Veggi from '../components/Veggi'
+import Popular from '../components/Popular'
+
+function Home() {
+  return (
+    <div>
+      <Veggi/>
+      <Popular/>
+    </div>
+  )
+}
+
+export default Home

5a1f606d78d9bfd34adb23861e7d8fe1eff4b8ba
adilummar <madilummer@gmail.com>
2023-02-21 08:15:08 +0530

    recipe page dealed

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,26 +1,95 @@
-import React,{useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Recipe() {
-    let params = useParams()
-    const [details,setDetails] = useState([])
-    
-useEffect(()=>{
-    fetchRecipe()
-},[])
+  let params = useParams();
+  const [details, setDetails] = useState([]);
+  const [activeTab, setActiveTab] = useState("instructions");
 
-    const fetchRecipe =async ()=>{
-        const api = await fetch(`
+  useEffect(() => {
+    fetchRecipe();
+  }, [params.name]);
+
+  const fetchRecipe = async () => {
+    const api = await fetch(`
             https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}
-        `)
-        const data = await api.json()
-        console.log(data)
-    } 
+        `);
+    const data = await api.json();
+    setDetails(data);
+    console.log(data);
+  };
   return (
-    <div>
-      recipe
-    </div>
-  )
+    <DetailedWrapped>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt="" />
+      </div>
+      <Info>
+        <Buttons
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => {
+            setActiveTab("instructions");
+          }}
+        >
+          instructions
+        </Buttons>
+        <Buttons
+          className={activeTab === "ingeredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingeredients")}
+        >
+          ingredients
+        </Buttons>
+        <div>
+          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+          <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+        </div>
+        <ul>
+          {details.extendedIngredients && details.extendedIngredients.map(ingredients=>{
+            return(
+              <li key={ingredients.id}>{ingredients.original}</li>
+            )
+          })}
+        </ul>
+      </Info>
+    </DetailedWrapped>
+  );
 }
 
-export default Recipe
+const DetailedWrapped = styled.div`
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+  }
+
+  ul {
+    margin-top: 2rem;
+  }
+
+  li {
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+  }
+`;
+
+const Buttons = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 5rem;
+`;
+
+export default Recipe;
